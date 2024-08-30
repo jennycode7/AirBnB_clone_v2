b82fc6fd55723a4c5a6ea833c8a51b4749cbb74d
jennycode7 <jennessecosmas7@gmail.com>
2024-08-29 18:28:07 -0700

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            selected = {}
+            for key, obj in FileStorage.__objects.items():
+                if isinstance(obj, cls):
+                    selected[key] = obj
+            return selected
+        else:
+            return FileStorage.__objects
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +32,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f, indent=2)
+
+    def delete(self, obj):
+        """ Deletes an Object """
+        if obj is not None:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
